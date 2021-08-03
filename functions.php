7c33d9d83cbab0fe95949b9e2086c5ce6fe01e41
takmorisawa <xs324884@sv10209.xserver.jp>
2021-08-04 00:43:41 +0900

    adjust size of site title and specify font-family in css

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style')
-);
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+add_action( 'wp_enqueue_scripts', function() {
+    $styles = wp_styles();
+    $styles->add_data( 'twentytwenty-style', 'after', array() );
+}, 20 );
 ?>
