ff9aa39b6cf4678c9e86d3e680165ab277d6a843
takmorisawa <xs324884@sv10209.xserver.jp>
2021-08-08 09:02:02 +0900

    move javascrip in function.php to assets/js/sg-customize.js

---
+++ assets/js/sg-customize.js
@@ -0,0 +1,76 @@
+function showNextWagashi() {
+    var date = new Date();
+    var month = date.getMonth();
+    var day = date.getDay();
+    var next_month = day > (16 - 1) ? month + 1 : month;
+    var targets = jQuery("div[class*=sg-monthly]");
+    var spacer = jQuery(".sg-next-wagashi");
+    if (spacer.length == 0) {
+        return;
+    }
+
+    target = jQuery(targets[next_month]).clone(true);
+    target.appendTo(spacer);
+
+    headers = jQuery(".sg-next-wagashi h3");
+    var header= jQuery(headers[0]);
+    var title = header.text();
+    header.text(title.replace("%s", String(month + 1)));
+    headers[1].remove();
+}
+
+function checkAndShowMonthly() {
+    var targets = jQuery(".sg-monthly");
+    targets.each(function(index, elm) {
+        var margin = 70;
+        var target = jQuery(elm);
+        var target_top = target.offset().top;
+        var win_height = jQuery(window).height();
+        var scroll_top = jQuery(window).scrollTop();
+        if(scroll_top - margin > target_top - win_height) {
+            target.toggleClass("sg-monthly sg-monthly-shown");
+        }
+    });
+}
+
+function getParam($name) {
+    var queryString = window.location.search;
+    var queryObject = new Object();
+    if(queryString){
+        queryString = queryString.substring(1);
+        var parameters = queryString.split("&");
+
+        for (var i = 0; i < parameters.length; i++) {
+            var element = parameters[i].split("=");
+
+            var paramName = decodeURIComponent(element[0]);
+            var paramValue = decodeURIComponent(element[1]);
+
+            queryObject[paramName] = paramValue;
+        }
+    }
+    return queryObject;
+}
+
+function switchLanguage() {
+
+    if (getParam()["lang"] == "en") {
+        var targets = jQuery(".sg-lang-en");
+        targets.each(function(index, elm) {
+            jQuery(elm).toggleClass("sg-lang-en sg-lang-ja-shown");
+        });
+    }
+    else {
+        var targets = jQuery(".sg-lang-ja");
+        targets.each(function(index, elm) {
+            jQuery(elm).toggleClass("sg-lang-ja sg-lang-ja-shown");
+        });
+    }
+}
+
+jQuery(window).load(function() {
+    switchLanguage();
+    showNextWagashi();
+    checkAndShowMonthly();
+})
+jQuery(window).scroll(function() { checkAndShowMonthly(); })
--- functions.php
+++ functions.php
@@ -19,8 +19,8 @@ function flushRules(){
     global $wp_rewrite;
     $wp_rewrite->flush_rules();
 }
-add_filter('rewrite_rules_array','wp_insertMyRewriteRules');
-function wp_insertMyRewriteRules($rules)
+add_filter('rewrite_rules_array', 'sg_insert_rewrite_rules');
+function sg_insert_rewrite_rules($rules)
 {
     $newrules = array();
     $newrules['en'] = 'index.php?lang=en';
@@ -34,90 +34,11 @@ function sg_add_google_fonts() {
 }
 add_action( 'wp_enqueue_scripts', 'sg_add_google_fonts' );
 
-function sg_add_typesquare_tag() {
+function sg_add_javascripts() {
     echo '<script type="text/javascript" src="//typesquare.com/3/tsst/script/ja/typesquare.js?6109e6210a344f8590530e37ac1e02e5" charset="utf-8"></script>';
-
-    $query='<script type="text/javascript">
-    function showNextWagashi() {
-        var date = new Date();
-        var month = date.getMonth();
-        var day = date.getDay();
-        var next_month = day > (16 - 1) ? month + 1 : month;
-        var targets = jQuery("div[class*=sg-monthly]");
-        var spacer = jQuery(".sg-next-wagashi");
-        if (spacer.length == 0) {
-            return;
-        }
-
-        target = jQuery(targets[next_month]).clone(true);
-        target.appendTo(spacer);
-
-        headers = jQuery(".sg-next-wagashi h3");
-        var header= jQuery(headers[0]);
-        var title = header.text();
-        header.text(title.replace("%s", String(month + 1)));
-        headers[1].remove();
-    }
-
-    function checkAndShowMonthly() {
-        var targets = jQuery(".sg-monthly");
-        targets.each(function(index, elm) {
-            var margin = 70;
-            var target = jQuery(elm);
-            var target_top = target.offset().top;
-            var win_height = jQuery(window).height();
-            var scroll_top = jQuery(window).scrollTop();
-            if(scroll_top - margin > target_top - win_height) {
-                target.toggleClass("sg-monthly sg-monthly-shown");
-            }
-        });
-    }
-
-    function getParam($name) {
-        var queryString = window.location.search;
-        var queryObject = new Object();
-        if(queryString){
-            queryString = queryString.substring(1);
-            var parameters = queryString.split("&");
-
-            for (var i = 0; i < parameters.length; i++) {
-                var element = parameters[i].split("=");
-
-                var paramName = decodeURIComponent(element[0]);
-                var paramValue = decodeURIComponent(element[1]);
-
-                queryObject[paramName] = paramValue;
-            }
-        }
-        return queryObject;
-    }
-
-    function switchLanguage() {
-
-        if (getParam()["lang"] == "en") {
-            var targets = jQuery(".sg-lang-en");
-            targets.each(function(index, elm) {
-                jQuery(elm).toggleClass("sg-lang-en sg-lang-ja-shown");
-            });
-        }
-        else {
-            var targets = jQuery(".sg-lang-ja");
-            targets.each(function(index, elm) {
-                jQuery(elm).toggleClass("sg-lang-ja sg-lang-ja-shown");
-            });
-        }
-    }
-
-    jQuery(window).load(function() {
-        switchLanguage();
-        showNextWagashi();
-        checkAndShowMonthly();
-    })
-    jQuery(window).scroll(function() { checkAndShowMonthly(); })
-</script>';
-    echo $query;
+    echo '<script type="text/javascript" src="' . get_stylesheet_directory_uri () . '/assets/js/sg-customize.js" charset="utf-8"></script>';
 }
-add_action( 'wp_head', 'sg_add_typesquare_tag' );
+add_action( 'wp_head', 'sg_add_javascripts' );
 
 function add_query_vars_filter( $vars ){
     $vars[] = "lang";
